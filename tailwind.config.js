d9686cba3b9ebf6a44ce44b00b6de4c238f1173e
milan reijmer <milan.reijmer@student.graafschapcollege.nl>
2024-06-06 14:20:01 +0200

    page gemaakt

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ export default {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: {
+                headerImgProducts:"url('/resources/images/product-header.png')",
+            },
+        },
     },
     plugins: [],
 };
