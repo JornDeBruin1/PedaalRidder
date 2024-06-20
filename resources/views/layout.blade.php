556b59fa016f263b20e84f3aa0cd7afbb7d4f5e9
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2024-06-20 14:58:16 +0200

    layout op gefrist

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -83,41 +83,6 @@
                 </div>
             </nav>
         </div>
-
-
-
-
-        {{-- <div class="w-full flex items-center justify-between sticky">
-            <div class="w-1/3 pl-4 pt-1 pb-1">
-                <a href="/">
-                    <img class="w-[18%]" src="{{ Vite::asset('resources/images/PedaalRidder_logo.png') }}" alt="">
-                </a>
-            </div>
-            <div class="w-1/3">
-                <ul class="flex justify-between">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="">Accessoires</a></li>
-                    <li><a href="">Reparatie</a></li>
-                    <li><a href="">Huren</a></li>
-                    <li><a href="/products">Kopen</a></li>
-                    <li><a href="/about-us">Over ons</a></li>
-
-                </ul>
-            </div>
-            <div class="w-1/3 flex justify-end">
-                <div class="p-3">
-                    <a href="">
-                        <i class="fa-regular fa-user"></i>
-                    </a>
-                </div>
-                <div class="p-3 pr-4">
-                    <a href="">
-                        <i class="fa-solid fa-cart-shopping"></i>
-                    </a>
-                </div>
-            </div>
-        </div> --}}
-
         @yield('content')
 
         <div class="w-full bg-[#22333B] pb-11 text-white flex">
