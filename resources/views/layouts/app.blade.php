a83977fe28acde38209c9934e1d83305a595d3e5
Nickolas Ciochina <nickolas.ciochina@student.graafschapcollege.nl>
2024-06-23 23:44:23 +0200

    layout and app.js changed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,51 +1,91 @@
-<!-- resources/views/layouts/app.blade.php -->
-
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta description="">
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" >
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta description="">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
+        <title>De PedaalRidder</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
 <body>
     <div class="w-full bg-[#F1F4F3]">
-        <div class="w-full flex items-center justify-between sticky">
-            <div class="w-1/3 pl-4 pt-1 pb-1">
-                <a href="/">
-                    <img class="w-[18%]" src="{{ Vite::asset('resources/images/PedaalRidder_logo.png') }}" alt="">
-                </a>
+        <nav class="relative px-4 py-4 flex justify-between items-center bg-white">
+            <a class="text-3xl font-bold leading-none" href="#">
+                <img class="w-[18%]" src="{{ Vite::asset('resources/images/PedaalRidder_logo.png') }}" alt="">
+            </a>
+            <div class="lg:hidden">
+                <button class="navbar-burger flex items-center text-blue-600 p-3">
+                    <svg class="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <title>hamburger menu</title>
+                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+                    </svg>
+                </button>
             </div>
-            <div class="w-1/3">
-                <ul class="flex justify-between">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="#">Accessoires</a></li>
-                    <li><a href="#">Reparatie</a></li>
-                    <li><a href="#">Huren</a></li>
-                    <li><a href="#">Kopen</a></li>
-                    <li><a href="#">Over ons</a></li>
-                </ul>
-            </div>
+            <ul class="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:items-center lg:w-auto lg:space-x-6">
+                <li><a class="text-sm hover:text-blue-600" href="/">Home</a></li>
+                <li><a class="text-sm hover:text-blue-600" href="/accessoires">Accessoires</a></li>
+                <li><a class="text-sm hover:text-blue-600" href="/reperatie">Reparatie</a></li>
+                <li><a class="text-sm hover:text-blue-600 " href="#">Huren</a></li>
+                <li><a class="text-sm hover:text-blue-600" href="/products">Kopen</a></li>
+                <li><a class="text-sm hover:text-blue-600" href="/about-us">Over ons</a></li>
+                <li class="text-gray-300"></li>
+            </ul>
             <div class="w-1/3 flex justify-end">
-                @if (Auth::check())
-                    <form method="POST" action="{{ route('logout') }}" class="inline">
-                        @csrf
-                        <button type="submit" class="p-3 text-red-600 hover:text-red-700 focus:outline-none"><i class="fa-regular fa-user"></i> Logout</button>
-                    </form>
-                @else
-                    <div class="p-3">
-                        <a href="/login"><i class="fa-regular fa-user"></i> Login</a>
-                    </div>
-                @endif
+                <div class="p-3">
+                    <a href="">
+                        <i class="fa-regular fa-user"></i>
+                    </a>
+                </div>
                 <div class="p-3 pr-4">
-                    <a href=""><i class="fa-solid fa-cart-shopping"></i></a>
+                    <a href="javascript:void(0)">
+                        <i class="fa-solid fa-cart-shopping"></i>
+                    </a>
                 </div>
             </div>
+            @include('cart-sheet')
         </div>
 
+        </nav>
+        <div class="navbar-menu relative z-50 hidden">
+            <div class="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
+            <nav class="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto">
+                <div class="flex items-center mb-8">
+                    <a class="mr-auto text-3xl font-bold leading-none" href="#">
+                        <img class="w-[18%]" src="{{ Vite::asset('resources/images/PedaalRidder_logo.png') }}" alt="">
+                    </a>
+                    <button class="navbar-close">
+                        <svg class="h-6 w-6 text-gray-400 cursor-pointer hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                        </svg>
+                    </button>
+                </div>
+                <div>
+                    <ul>
+                        <li class="mb-1">
+                            <a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="/">Home</a>
+                        </li>
+                        <li class="mb-1">
+                            <a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Accessoires</a>
+                        </li>
+                        <li class="mb-1">
+                            <a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="/reperatie">Reparatie</a>
+                        </li>
+                        <li class="mb-1">
+                            <a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Huren</a>
+                        </li>
+                        <li class="mb-1">
+                            <a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="/products">Kopen</a>
+                        </li>
+                        <li class="mb-1">
+                            <a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="/about-us">Over ons</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
         @yield('content')
 
         <div class="w-full bg-[#22333B] pb-11 text-white flex">
@@ -53,10 +93,10 @@
                 <ul class="p-4 flex flex-col">
                     <li><a href="/">Home</a></li>
                     <li><a href="#">Accessoires</a></li>
-                    <li><a href="#">Reparatie</a></li>
+                    <li><a href="/reperatie">Reparatie</a></li>
                     <li><a href="#">Huren</a></li>
-                    <li><a href="#">Kopen</a></li>
-                    <li><a href="#">Over ons</a></li>
+                    <li><a href="/products">Kopen</a></li>
+                    <li><a href="/about-us">Over ons</a></li>
                 </ul>
             </div>
             <div class="w-2/3">
@@ -72,6 +112,7 @@
                                 <input id="link-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                                 <label for="link-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" class="underline">terms and conditions</a>.</label>
                             </div>
+
                         </div>
                     </form>
                 </div>
